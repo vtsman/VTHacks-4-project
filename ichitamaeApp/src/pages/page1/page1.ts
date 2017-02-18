4f6d1d07e5e09e256b7efc64d1847c1faaab3740
Brandon Fan <brandonfan1256@gmail.com>
2017-02-18 03:00:53 -0500

    Add basic information from http request

--- ichitamaeApp/src/pages/page1/page1.ts
+++ ichitamaeApp/src/pages/page1/page1.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
-export class Page1 {
-
-  constructor(public navCtrl: NavController) {
-    
+export class Page1 implements OnInit {
+  url: string = 'http://172.30.111.135:3000/recipe';
+  data = {};
+  ingredients;
+  loadData() {
+    return this.http
+      .get(this.url)
+      .subscribe((data) => {
+        this.data = (JSON.parse(data[ "_body" ]));
+        console.log(this.data);
+        this.ingredients = this.data[ 'ingredients' ];
+        console.log(this.ingredients);
+      });
   }
-
+  constructor(public navCtrl: NavController, private http: Http) {
+  }
+  ngOnInit() {
+    this.loadData();
+  }
 }
