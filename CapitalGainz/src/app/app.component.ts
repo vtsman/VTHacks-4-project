d5c04e3a8c09acd7683141ef98691ffc71954807
Brandon Fan <brandonfan1256@gmail.com>
2017-02-19 06:21:47 -0500

    delete pages 1 and 2

--- CapitalGainz/src/app/app.component.ts
+++ CapitalGainz/src/app/app.component.ts
@@ -23,8 +23,6 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 },
       { title: 'Home', component: HomePage },
       { title: 'Classification', component: ClassifierPage }
     ];
--- CapitalGainz/src/app/app.module.ts
+++ CapitalGainz/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 import { HomePage } from '../pages/home/home';
 import { Data } from '../providers/data';
 import { ClassifierPage } from '../pages/classifier/classifier';
@@ -10,8 +8,6 @@ import { ClassifierPage } from '../pages/classifier/classifier';
 
 var importData = [
   MyApp,
-  Page1,
-  Page2,
   HomePage,
   ClassifierPage
 ]
