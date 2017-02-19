582f27b25ef2e1eb633a5d1fa2aac93fb90d1ee5
Brandon Fan <brandonfan1256@gmail.com>
2017-02-19 04:47:24 -0500

    finish generation of training data for classifier

--- CapitalGainz/data/generate_classifier.py
+++ CapitalGainz/data/generate_classifier.py
@@ -1,13 +1,20 @@
 import json
 import csv
-
+values = []
 with open('top25capital.json') as data_file:
     data = json.load(data_file)
 for i in data:
     string = ''
     for x in i:
-        if x != 'type':
-            string += str(i[x]) + ','
+        if i != 'type':
+            string += str(data[i]) + ','
         else:
-            string += str(i[x])
-    print string
+            string += str(data[i])
+        print string
+
+with open('nlp_classification.csv', 'wb') as csvfile:
+    fieldnames = ['data', 'classification']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for val in values:
+        writer.writerow({'data': val, 'classification': ''})
